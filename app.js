cb236b758722e33e56d51c4526871d9e77acf3ab
mshalom27 <menshalom07@gmail.com>
2025-06-18 12:27:13 +0400

    backend

--- app.js
+++ app.js
@@ -23,8 +23,45 @@ app.get('/', (req, res) => {
 
 io.on('connection', (uniquesocket) => {
   console.log('A user connected');
-})
+
+  if (!players.white) {
+    players.white = uniquesocket.id;
+    uniquesocket.emit('playerRole', 'White');
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit('playerRole', 'Black');
+  }else{
+    uniquesocket.emit('playerRole', 'SpectatorRole');
+  }
+
+  uniquesocket.on("disconnect", () => {
+    if(uniquesocket.id === players.white){
+      delete players.white;
+    }else if (uniquesocket.id === players.black){
+      delete players.black;
+    }
+});
+
+uniquesocket.on("move", (move) => {
+  try{
+    if(chess.turn() === "w" && socket.id !== players.white) return;
+    if(chess.turn() === "b" && socket.id !== players.black) return;
+    const result = chess.move(move);
+
+    if(result){
+      currentPlayer = chess.turn();
+      io.emit("move",move);
+      io.emit("boardstate", chess.fen()); 
+    }else{
+      console.log("Invalid move : ",move);
+      uniquesocket.emit("invalidMove", move);
+    }
+  }catch(err){
+    uniquesocket.emit("Invalid move : ", move);
+  }
+});
+});
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
-});
+})
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,68 @@
 const socket = io();
+const chess = new chess();
+const boardElement = document.querySelector('.chessboard');
 
-socket.emit('joinRoom');
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = '';
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = '';
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement('div');
+      squareElement.classList.add('square', (rowindex + square%2 === 0 ? 'light' : 'dark'));
+      
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = squareindex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add('piece', square.color === "w" ? "white" : "black");
+        
+        pieceElement.innerText = "";
+        pieceElement.draggable = playerRole === square.color;
+
+        pieceElement.addEventListener('dragstart', (event) => {
+
+           if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = {row : rowindex, col: squareindex};
+        }
+
+          draggedPiece = pieceElement;
+          sourceSquare = squareElement;
+          event.dataTransfer.setData('text/plain', '');
+        });
+
+      }
+
+      boardElement.appendChild(square);
+    });
+  });
+}
+
+const handleMove = (event) => {
+  const targetSquare = event.target;
+  if (draggedPiece && targetSquare.classList.contains('square')) {
+    const move = `${sourceSquare.dataset.position}${targetSquare.dataset.position}`;
+    socket.emit('move', move);
+  }
+};
+
+const getPieceUnicode = (piece) => {
+  if (piece === 'wP') return '\u2659';
+  if (piece === 'wR') return '\u2656';
+  if (piece === 'wN') return '\u2658';
+  if (piece === 'wB') return '\u2657';
+  if (piece === 'wQ') return '\u2655';
+  if (piece === 'wK') return '\u2654';
+  if (piece === 'bP') return '\u265F';
+  if (piece === 'bR') return '\u265C';
+  if (piece === 'bN') return '\u265E';
+  if (piece === 'bB') return '\u265D';
+  if (piece === 'bQ') return '\u265B';
+  if (piece === 'bK') return '\u265A';
+  return '';
+};
